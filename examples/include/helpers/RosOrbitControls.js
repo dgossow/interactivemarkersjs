250561909ef7b48ef660417d2d9a80c97396cb99
David Gossow <dgossow@willowgarage.com>
2012-12-10 17:30:43 -0800

    added camera move function to orbit controls

--- examples/include/helpers/RosOrbitControls.js
+++ examples/include/helpers/RosOrbitControls.js
@@ -8,287 +8,338 @@
 // behaves like THREE.OrbitControls, but uses
 // right-handed coords and z as up vector
 
-THREE.RosOrbitControls = function ( object, domElement ) {
+THREE.RosOrbitControls = function(object, domElement) {
 
-	THREE.EventTarget.call( this );
+  THREE.EventTarget.call(this);
 
-	this.object = object;
-	
-	// hack the tracked object (camera) to be right-handed with z=up
-  this.object.scale.x=-1;
-  this.object.scale.y=-1;
-  this.object.up = new THREE.Vector3( 0, 0, -1 );
-  
-	this.domElement = ( domElement !== undefined ) ? domElement : document;
+  this.object = object;
 
-	// API
+  // hack the tracked object (camera) to be right-handed with z=up
+  this.object.scale.x = -1;
+  this.object.scale.y = -1;
+  this.object.up = new THREE.Vector3(0, 0, -1);
 
-	this.center = new THREE.Vector3();
+  this.domElement = (domElement !== undefined ) ? domElement : document;
 
-	this.userZoom = true;
-	this.userZoomSpeed = 1.0;
+  // API
 
-	this.userRotate = true;
-	this.userRotateSpeed = 1.0;
+  this.center = new THREE.Vector3();
 
-	this.autoRotate = false;
-	this.autoRotateSpeed = 2.0; // 30 seconds per round when fps is 60
+  this.userZoom = true;
+  this.userZoomSpeed = 1.0;
 
-	// internals
+  this.userRotate = true;
+  this.userRotateSpeed = 1.0;
 
-	var scope = this;
+  this.autoRotate = false;
+  this.autoRotateSpeed = 2.0;
+  // 30 seconds per round when fps is 60
 
-	var EPS = 0.000001;
-	var PIXELS_PER_ROUND = 1800;
+  // internals
 
-	var rotateStart = new THREE.Vector2();
-	var rotateEnd = new THREE.Vector2();
-	var rotateDelta = new THREE.Vector2();
+  var scope = this;
 
-	var zoomStart = new THREE.Vector2();
-	var zoomEnd = new THREE.Vector2();
-	var zoomDelta = new THREE.Vector2();
+  var EPS = 0.000001;
+  var PIXELS_PER_ROUND = 1800;
 
-	var phiDelta = 0;
-	var thetaDelta = 0;
-	var scale = 1;
+  var rotateStart = new THREE.Vector2();
+  var rotateEnd = new THREE.Vector2();
+  var rotateDelta = new THREE.Vector2();
 
-	var lastPosition = new THREE.Vector3();
+  var zoomStart = new THREE.Vector2();
+  var zoomEnd = new THREE.Vector2();
+  var zoomDelta = new THREE.Vector2();
 
-	var STATE = { NONE : -1, ROTATE : 0, ZOOM : 1 };
-	var state = STATE.NONE;
+  var moveStartCenter = new THREE.Vector3();
+  var moveStartNormal = new THREE.Vector3();
+  var moveStartPosition = new THREE.Vector3();
+  var moveStartIntersection = new THREE.Vector3();
 
-	// events
+  var phiDelta = 0;
+  var thetaDelta = 0;
+  var scale = 1;
 
-	var changeEvent = { type: 'change' };
+  var lastPosition = new THREE.Vector3();
 
+  var STATE = {
+    NONE : -1,
+    ROTATE : 0,
+    ZOOM : 1,
+    MOVE : 2
+  };
+  var state = STATE.NONE;
 
-	this.rotateLeft = function ( angle ) {
+  // events
 
-		if ( angle === undefined ) {
+  var changeEvent = {
+    type : 'change'
+  };
 
-			angle = getAutoRotationAngle();
+  this.rotateLeft = function(angle) {
 
-		}
+    if (angle === undefined) {
 
-		thetaDelta -= angle;
+      angle = getAutoRotationAngle();
 
-	};
+    }
 
-	this.rotateRight = function ( angle ) {
+    thetaDelta -= angle;
 
-		if ( angle === undefined ) {
+  };
 
-			angle = getAutoRotationAngle();
+  this.rotateRight = function(angle) {
 
-		}
+    if (angle === undefined) {
 
-		thetaDelta += angle;
+      angle = getAutoRotationAngle();
 
-	};
+    }
 
-	this.rotateUp = function ( angle ) {
+    thetaDelta += angle;
 
-		if ( angle === undefined ) {
+  };
 
-			angle = getAutoRotationAngle();
+  this.rotateUp = function(angle) {
 
-		}
+    if (angle === undefined) {
 
-		phiDelta -= angle;
+      angle = getAutoRotationAngle();
 
-	};
+    }
 
-	this.rotateDown = function ( angle ) {
+    phiDelta -= angle;
 
-		if ( angle === undefined ) {
+  };
 
-			angle = getAutoRotationAngle();
+  this.rotateDown = function(angle) {
 
-		}
+    if (angle === undefined) {
 
-		phiDelta += angle;
+      angle = getAutoRotationAngle();
 
-	};
+    }
 
-	this.zoomIn = function ( zoomScale ) {
+    phiDelta += angle;
 
-		if ( zoomScale === undefined ) {
+  };
 
-			zoomScale = getZoomScale();
+  this.zoomIn = function(zoomScale) {
 
-		}
+    if (zoomScale === undefined) {
 
-		scale /= zoomScale;
+      zoomScale = getZoomScale();
 
-	};
+    }
 
-	this.zoomOut = function ( zoomScale ) {
+    scale /= zoomScale;
 
-		if ( zoomScale === undefined ) {
+  };
 
-			zoomScale = getZoomScale();
+  this.zoomOut = function(zoomScale) {
 
-		}
+    if (zoomScale === undefined) {
 
-		scale *= zoomScale;
+      zoomScale = getZoomScale();
 
-	};
+    }
 
-	this.update = function () {
+    scale *= zoomScale;
 
-		var position = this.object.position;
-		var offset = position.clone().subSelf( this.center )
+  };
+
+  this.update = function() {
+
+    var position = this.object.position;
+    var offset = position.clone().subSelf(this.center)
 
     // x->y, y->z, z->x
 
-		// angle from z-axis around y-axis
-		var theta = Math.atan2( offset.y, offset.x );
+    // angle from z-axis around y-axis
+    var theta = Math.atan2(offset.y, offset.x);
 
-		// angle from y-axis
+    // angle from y-axis
 
-		var phi = Math.atan2( Math.sqrt( offset.y * offset.y + offset.x * offset.x ), offset.z );
+    var phi = Math.atan2(Math.sqrt(offset.y * offset.y + offset.x * offset.x), offset.z);
 
-		if ( this.autoRotate ) {
+    if (this.autoRotate) {
 
-			this.rotateLeft( getAutoRotationAngle() );
+      this.rotateLeft(getAutoRotationAngle());
 
-		}
+    }
 
-		theta += thetaDelta;
-		phi += phiDelta;
+    theta += thetaDelta;
+    phi += phiDelta;
 
-		// restrict phi to be betwee EPS and PI-EPS
+    // restrict phi to be betwee EPS and PI-EPS
 
-		phi = Math.max( EPS, Math.min( Math.PI - EPS, phi ) );
+    phi = Math.max(EPS, Math.min(Math.PI - EPS, phi));
 
-		var radius = offset.length();
-		offset.y = radius * Math.sin( phi ) * Math.sin( theta );
-		offset.z = radius * Math.cos( phi );
-		offset.x = radius * Math.sin( phi ) * Math.cos( theta );
-		offset.multiplyScalar( scale );
+    var radius = offset.length();
+    offset.y = radius * Math.sin(phi) * Math.sin(theta);
+    offset.z = radius * Math.cos(phi);
+    offset.x = radius * Math.sin(phi) * Math.cos(theta);
+    offset.multiplyScalar(scale);
 
-		position.copy( this.center ).addSelf( offset );
+    position.copy(this.center).addSelf(offset);
 
-		this.object.lookAt( this.center );
+    this.object.lookAt(this.center);
 
-		thetaDelta = 0;
-		phiDelta = 0;
-		scale = 1;
+    thetaDelta = 0;
+    phiDelta = 0;
+    scale = 1;
 
-		if ( lastPosition.distanceTo( this.object.position ) > 0 ) {
+    if (lastPosition.distanceTo(this.object.position) > 0) {
 
-			this.dispatchEvent( changeEvent );
+      this.dispatchEvent(changeEvent);
 
-			lastPosition.copy( this.object.position );
+      lastPosition.copy(this.object.position);
 
-		}
-	};
+    }
+  };
 
+  function getAutoRotationAngle() {
 
-	function getAutoRotationAngle() {
+    return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
 
-		return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
+  }
 
-	}
+  function getZoomScale() {
 
-	function getZoomScale() {
+    return Math.pow(0.95, scope.userZoomSpeed);
 
-		return Math.pow( 0.95, scope.userZoomSpeed );
+  }
 
-	}
+  function intersectViewPlane(mouseRay, planeOrigin, planeNormal) {
 
-	function onMouseDown( event3d ) {
-	  
-	  var event = event3d.domEvent;
+    var vector = new THREE.Vector3();
+    var intersection = new THREE.Vector3();
 
-		event.preventDefault();
+    vector.sub(planeOrigin, mouseRay.origin);
+    dot = mouseRay.direction.dot(planeNormal);
 
-		if ( event.button === 0 || event.button === 2 ) {
+    // bail if ray and plane are parallel
+    if (Math.abs(dot) < mouseRay.precision)
+      return null;
 
-			state = STATE.ROTATE;
+    // calc distance to plane
+    scalar = planeNormal.dot(vector) / dot;
 
-			rotateStart.set( event.clientX, event.clientY );
+    intersection = mouseRay.direction.clone().multiplyScalar( scalar );
+    return intersection;
+  };
+  
 
-		} else if ( event.button === 1 ) {
-
-			state = STATE.ZOOM;
-
-			zoomStart.set( event.clientX, event.clientY );
-
-		}
-
-	}
-
-	function onMouseMove( event3d ) {
-
-		    var event = event3d.domEvent;
-
-
-		if ( state === STATE.ROTATE ) {
-
-			rotateEnd.set( event.clientX, event.clientY );
-			rotateDelta.sub( rotateEnd, rotateStart );
-
-			scope.rotateLeft( 2 * Math.PI * rotateDelta.x / PIXELS_PER_ROUND * scope.userRotateSpeed );
-			scope.rotateUp( 2 * Math.PI * rotateDelta.y / PIXELS_PER_ROUND * scope.userRotateSpeed );
-
-			rotateStart.copy( rotateEnd );
-
-		} else if ( state === STATE.ZOOM ) {
-
-			zoomEnd.set( event.clientX, event.clientY );
-			zoomDelta.sub( zoomEnd, zoomStart );
-
-			if ( zoomDelta.y > 0 ) {
-
-				scope.zoomIn();
-
-			} else {
-
-				scope.zoomOut();
-
-			}
-
-			zoomStart.copy( zoomEnd );
-
-		}
-
-	}
-
-	function onMouseUp( event3d ) {
+  function onMouseDown(event3d) {
 
     var event = event3d.domEvent;
 
-		if ( ! scope.userRotate ) return;
+    event.preventDefault();
 
-		state = STATE.NONE;
+    switch( event.button ) {
+      case 0:
+        state = STATE.ROTATE;
+        rotateStart.set(event.clientX, event.clientY);
+        break;
+      case 2:
+        state = STATE.MOVE;
+        
+        moveStartNormal = new THREE.Vector3(0,0,1);
+        var rMat = new THREE.Matrix4().extractRotation( object.matrix );
+        rMat.multiplyVector3( moveStartNormal );
+        
+        moveStartCenter=scope.center.clone();
+        moveStartPosition=scope.object.position.clone();
+        moveStartIntersection=intersectViewPlane(event3d.mouseRay, moveStartCenter, moveStartNormal);
+        break;
+      case 1:
+        state = STATE.ZOOM;
+        zoomStart.set(event.clientX, event.clientY);
+        break;
+    }
 
-	}
-
-	function onMouseWheel( event3d ) {
-
-		if ( ! scope.userZoom ) return;
+  }
+  
+  function onMouseMove(event3d) {
 
     var event = event3d.domEvent;
 
-		if ( event.wheelDelta > 0 ) {
+    if (state === STATE.ROTATE) {
 
-			scope.zoomOut();
+      rotateEnd.set(event.clientX, event.clientY);
+      rotateDelta.sub(rotateEnd, rotateStart);
 
-		} else {
+      scope.rotateLeft(2 * Math.PI * rotateDelta.x / PIXELS_PER_ROUND * scope.userRotateSpeed);
+      scope.rotateUp(2 * Math.PI * rotateDelta.y / PIXELS_PER_ROUND * scope.userRotateSpeed);
 
-			scope.zoomIn();
+      rotateStart.copy(rotateEnd);
 
-		}
+    } else if (state === STATE.ZOOM) {
 
-	}
-	
-	THREE.EventTarget.call(this);
-  this.addEventListener('mousedown',onMouseDown);
-  this.addEventListener('mouseup',onMouseUp);
-  this.addEventListener('mousemove',onMouseMove);
-  this.addEventListener('mousewheel',onMouseWheel);
+      zoomEnd.set(event.clientX, event.clientY);
+      zoomDelta.sub(zoomEnd, zoomStart);
+
+      if (zoomDelta.y > 0) {
+
+        scope.zoomIn();
+
+      } else {
+
+        scope.zoomOut();
+
+      }
+
+      zoomStart.copy(zoomEnd);
+
+    } else if (state === STATE.MOVE) {
+
+      var intersection = intersectViewPlane( event3d.mouseRay, scope.center, moveStartNormal );
+      
+      if ( !intersection ) return;
+      
+      var delta = new THREE.Vector3().sub( moveStartIntersection.clone(), intersection.clone() );
+      
+      scope.center.add( moveStartCenter.clone(), delta.clone() );
+      scope.object.position.add( moveStartPosition.clone(), delta.clone() );
+      scope.update();
+      scope.object.updateMatrixWorld();
+    }
+
+  }
+
+  function onMouseUp(event3d) {
+
+    if (!scope.userRotate)
+      return;
+
+    state = STATE.NONE;
+
+  }
+
+  function onMouseWheel(event3d) {
+
+    if (!scope.userZoom)
+      return;
+
+    var event = event3d.domEvent;
+
+    if (event.wheelDelta > 0) {
+
+      scope.zoomOut();
+
+    } else {
+
+      scope.zoomIn();
+
+    }
+
+  }
+
+
+  THREE.EventTarget.call(this);
+  this.addEventListener('mousedown', onMouseDown);
+  this.addEventListener('mouseup', onMouseUp);
+  this.addEventListener('mousemove', onMouseMove);
+  this.addEventListener('mousewheel', onMouseWheel);
   //this.onmousedown = function(event){ onMouseDown(event); }
   //this.onmouseup = function(event){ onMouseUp(event); }
   //this.onmousemove = function(event){ onMouseMove(event); }
